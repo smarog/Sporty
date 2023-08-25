5fe19f0a5a7a5295c501848a1198f7efa733f80a
smarog <smaro13@windowslive.com>
2023-08-25 20:18:23 +0300

    Initial Commit

---
+++ SportyUITests/SportyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SportyUITestsLaunchTests.swift
+//  SportyUITests
+//
+//  Created by Smaro Goulianou on 25/8/23.
+//
+
+import XCTest
+
+final class SportyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
